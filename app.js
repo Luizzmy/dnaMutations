f22a64603a12b25304769a34a76ec15ab83dff7b
Luizzmy <luisg.valenzuelam@gmail.com>
2021-01-11 09:39:22 +0200

    remote db setup

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/dnamutations', {useNewUrlParser: true})
+  .connect(process.env.DB || 'mongodb://localhost/dnamutations', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
